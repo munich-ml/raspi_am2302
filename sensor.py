b4264b52311cf218a8e1b84bf5cc5bf50d08ad8c
Holger <holger.steffens@gmail.com>
2022-11-27 14:51:00 +0100

    debugging

--- sensor.py
+++ sensor.py
@@ -25,28 +25,48 @@ if not os.path.isfile(fp):
     with open(fp, "w") as file:
         file.write(header)
         
-# prepare new line in logfile
-line = dt.datetime.now().strftime("%Y%m%d_%H%M%S")
+pi = pigpio.pi()
 
-# acquire sensor data
-try: 
-    pi = pigpio.pi()
+while True:
+    line = dt.datetime.now().strftime("%Y%m%d_%H%M%S")
     for sen in SENSORS:
         s = DHT22.sensor(pi, sen)
         s.trigger()
         time.sleep(0.2)
 
-        #print("{} {} {} {:3.2f} {} {} {} {}".format(
-        #    r, s.humidity(), s.temperature(), s.staleness(),
-        #    s.bad_checksum(), s.short_message(), s.missing_message(),
-        #    s.sensor_resets()))
-        line += ",{:.1f}".format(s.temperature())
-        line += ",{:.1f}".format(s.humidity())
+        logging.debug("### Sensor " + str(sen))
+        logging.debug("{} {} {:3.2f} {} {} {} {}".format(
+            s.humidity(), s.temperature(), s.staleness(),
+            s.bad_checksum(), s.short_message(), s.missing_message(),
+            s.sensor_resets()))
+        #line += "\t{:.1f}".format(s.temperature())
+        #line += ",{:.1f}".format(s.humidity())
         s.cancel()
-except Exception as e:
-    logging.error(str(e))        
-else:
-    with open(fp, "a") as file:
-        file.write(line+"\n")
-finally:
-    pi.stop()
+    logging.debug(line)
+    time.sleep(4)
+
+# # prepare new line in logfile
+# line = dt.datetime.now().strftime("%Y%m%d_%H%M%S")
+# 
+# # acquire sensor data
+# try: 
+#     pi = pigpio.pi()
+#     for sen in SENSORS:
+#         s = DHT22.sensor(pi, sen)
+#         s.trigger()
+#         time.sleep(0.2)
+# 
+#         #print("{} {} {} {:3.2f} {} {} {} {}".format(
+#         #    r, s.humidity(), s.temperature(), s.staleness(),
+#         #    s.bad_checksum(), s.short_message(), s.missing_message(),
+#         #    s.sensor_resets()))
+#         line += ",{:.1f}".format(s.temperature())
+#         line += ",{:.1f}".format(s.humidity())
+#         s.cancel()
+# except Exception as e:
+#     logging.error(str(e))        
+# else:
+#     with open(fp, "a") as file:
+#         file.write(line+"\n")
+# finally:
+#     pi.stop()
